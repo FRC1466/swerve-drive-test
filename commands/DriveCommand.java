8ad00b805dead6b967ab7444165c5b98fd2fe8bd
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2022-11-11 15:18:59 -0500

    remove unused imports

--- commands/DriveCommand.java
+++ commands/DriveCommand.java
@@ -5,11 +5,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 import java.lang.Math;
 
