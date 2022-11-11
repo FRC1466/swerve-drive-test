8bf48a6479fc695899a915990dc634a15bf81199
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2022-11-11 15:00:12 -0500

    reformat constants

--- robot/Constants.java
+++ robot/Constants.java
@@ -32,28 +32,33 @@ public final class Constants {
   }
 
   public static final class DriveConstants {
-    public static final int FRONTRIGHT_PORT_DRIVE = 3;
-    public static final int FRONTRIGHT_PORT_ROTATE = 4;
-    public static final int FRONTLEFT_PORT_DRIVE = 1;
-    public static final int FRONTLEFT_PORT_ROTATE = 2;
-    public static final int BACKRIGHT_PORT_DRIVE = 5;
-    public static final int BACKRIGHT_PORT_ROTATE = 6;
-    public static final int BACKLEFT_PORT_DRIVE = 7;
-    public static final int BACKLEFT_PORT_ROTATE = 8;
+    public static final int 
+      FRONTRIGHT_PORT_DRIVE = 3,
+      FRONTRIGHT_PORT_ROTATE = 4,
+      FRONTLEFT_PORT_DRIVE = 1,
+      FRONTLEFT_PORT_ROTATE = 2,
+      BACKRIGHT_PORT_DRIVE = 5,
+      BACKRIGHT_PORT_ROTATE = 6,
+      BACKLEFT_PORT_DRIVE = 7,
+      BACKLEFT_PORT_ROTATE = 8;
 
-    public static final double TRACKWIDTH_METERS = 0.375;
+    public static final double 
+      TRACKWIDTH_METERS = 0.375;
 
     // Drive limiters
-    public static final double PERCENT_ACTIVE_DEFAULT = 0.75;
-    public static final double PERCENT_ACTIVE = 0.20;
-    public static final double PERCENT_ACTIVE_PID = 0.8;
-    public static final double PERCENT_ACTIVE_PID_DEFAULT = 1.2;
+
+    public static final double 
+      LIMIT_VX = 1.0,
+      LIMIT_VY = 1.0,
+      LIMIT_ROT = 1.0;
+
 
   }
 
   public static final class OIConstants {
-    public static final int DRIVER_PORT = 0;
-    public static final int INTAKE_PORT = 1;
+    public static final int 
+      DRIVER_PORT = 0,
+      INTAKE_PORT = 1;
   }
 
   public static final class AutoConstants {
@@ -65,9 +70,10 @@ public final class Constants {
   }
 
   public static final class PIDConstants {
-    public static final int SLOT_IDX = 0;
-    public static final int PID_LOOP_IDX = 0;
-    public static final int TIMEOUT_MS = 30;
+    public static final int 
+      SLOT_IDX = 0,
+      PID_LOOP_IDX = 0,
+      TIMEOUT_MS = 30;
 
     //                                                    kP   	 kI    kD      kF          Iz    PeakOut
     public final static Gains DRIVE_GAINS_VELOCITY  = new Gains(0.2, 0.0001, 4.0, 0,  0,  0.6);
